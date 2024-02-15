ac03ab0ce405c53c2e18e255a30cf6366fe97d70
1352543 <146123056+1352543@users.noreply.github.com>
2024-02-15 08:41:29 -0500

    Add files via upload

--- restaurant.py
+++ restaurant.py
@@ -2,57 +2,58 @@
 #Andrew Fisher
 #Restaurant
 
-vegetarian = False
-vegan = False
-gluten_free = False
+search = "yes"
 
-response1 = input("Is anyone in your party vegetarian? ")
-if response1 == "yes":
-    vegetarian = True
-elif response1 == "no":
+while search == "yes":
+
     vegetarian = False
-
-response2 = input("Is anyone in your party vegan? ")
-if response2 == "yes":
-    vegan = True
-elif response2 == "no":
     vegan = False
-
-response3 = input("Is anyone in your party gluten free? ")
-if response3 == "yes":
-    gluten_free = True
-elif response3 == "no":
     gluten_free = False
 
+    response1 = input("Is anyone in your party vegetarian? (enter 'yes' or 'no') ")
+    if response1 == "yes":
+        vegetarian = True
 
-print("Here are your restaurant choices:")    
-if vegetarian:
-    if vegan:
+    response2 = input("Is anyone in your party vegan? (enter 'yes' or 'no') ")
+    if response2 == "yes":
+        vegan = True
+
+    response3 = input("Is anyone in your party gluten free? (enter 'yes' or 'no') ")
+    if response3 == "yes":
+        gluten_free = True
+
+    print("Here are your restaurant choices:") #Determines restaurant   
+    if vegetarian:
+        if vegan:
+            if gluten_free or not gluten_free:
+                print("Corner Café")
+                print("The Chef's Kitchen")
+        elif gluten_free:
+            print("Main Street Pizza Company")
+            print("Corner Café")
+            print("The Chef's Kitchen")
+        else:
+            print("Mama's Fine Italian")
+            print("Main Street Pizza Company")
+            print("Corner Café")
+            print("The Chef's Kitchen")
+                
+    elif vegan: #vegetarian = no
         if gluten_free or not gluten_free:
             print("Corner Café")
             print("The Chef's Kitchen")
-    elif gluten_free:
-        print("Main Street Pizza Company")
-        print("Corner Café")
-        print("The Chef's Kitchen")
-    else:
-        print("Mama's Fine Italian")
-        print("Main Street Pizza Company")
-        print("Corner Café")
-        print("The Chef's Kitchen")
-elif vegan: #vegetarian = no
-        if gluten_free or not gluten_free:
+            
+    else: #vegan = no
+        if gluten_free:
+            print("Main Street Pizza Company")
             print("Corner Café")
             print("The Chef's Kitchen")
-else: #vegan = no
-    if gluten_free:
-        print("Main Street Pizza Company")
-        print("Corner Café")
-        print("The Chef's Kitchen")
-    else:
-        print("Joe's Gourmet Burgers")
-        print("Mama's Fine Italian")
-        print("Main Street Pizza Company")
-        print("Corner Café")
-        print("The Chef's Kitchen")
+        else:
+            print("Joe's Gourmet Burgers")
+            print("Mama's Fine Italian")
+            print("Main Street Pizza Company")
+            print("Corner Café")
+            print("The Chef's Kitchen")
+
+    search = input("Enter 'yes' if you would like to do another restaurant search (enter 'no' to end) ")
 
