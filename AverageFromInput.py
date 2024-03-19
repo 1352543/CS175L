103ed80aede63fcd0dc78f1e2a03b3f072320276
1352543 <146123056+1352543@users.noreply.github.com>
2024-03-19 10:58:54 -0400

    Add files via upload

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -6,17 +6,27 @@ def main():
     total = 0
     num = 0
     totalnums = 0
-    num_file = open('numbers.txt', 'r')
-    for i in num_file:
-        totalnums += 1
-        num = float(i)
-        total = total + num
-        print(f'I read in  {totalnums} number(s) Current number is:   {num:6.2f} Total is:   {total:6.2f}')
+    
+    try:
+        num_file = open('numbers.txt', 'r')
+        for i in num_file:
+            try:
+                num = float(i)
+                totalnums += 1
+                total = total + num
+                print(f'I read in  {totalnums} number(s) Current number is:   {num:6.2f} Total is:   {total:6.2f}')
+            except ValueError:
+                i = i.strip()
+                print(f'Bad data: {i} skipping')
+                
+        num_file.close()
 
-    average = total / totalnums
-    print(f'Average is: {average}')
+        average = total / totalnums
+        print(f'Average is: {average}')
 
-    num_file.close()
+    except IOError:
+        import sys
+        sys.exit("File not found: numbers.txt - exiting")
 
 
 if __name__ == '__main__':
